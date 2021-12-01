2ed7a726927f42073e1cc52b90ff6f353d3e4b55
park ye jin <dyddyd0514@naver.com>
2021-12-01 18:42:15 +0900

    [feat] pdf upload page

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-app_name = 'functions'
+app_name = 'main'
 
 urlpatterns = [
-    path('',views.main, name='main')
+    path('',views.main, name='main'),
+    path('pdf_upload',views.model_form_upload,name='pdf_upload')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import PDFUploadForm
 # Create your views here.
 
 def main(request):
   return render(request,'main.html')
+
+def model_form_upload(request):
+  if request.method == 'POST':
+      form = PDFUploadForm(request.POST, request.FILES)
+      if form.is_valid():
+          form.save()
+          return redirect('main:main')
+  else:
+      form = PDFUploadForm()
+  return render(request, 'pdf_upload.html', {
+      'form': form
+  })
