f5344d29529948ce63a0e2b9d4e07fcc68c5b468
Kim Seohyun <kshjessica@ewhain.net>
2021-12-01 14:51:03 +0900

    chore/env settings

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Explanation(models.Model):
+    def __str__(self):
+        return self.name
+class MLModel(models.Model):
+    def __str__(self):
+        return self.name
+
+class MLResult(models.Model):
+    def __str__(self):
+        return self.name
+
---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from websec import views
+
+app_name = 'functions'
+
+urlpatterns = [
+    
+]
