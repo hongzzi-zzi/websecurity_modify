d549b89035d6189f27c8f7133142bf76ae4feb5d
park ye jin <dyddyd0514@naver.com>
2021-12-01 18:41:25 +0900

    [feat] pdf upload forms

---
+++ main/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.db import models
+from .models import PDFdocuments
+
+class PDFUploadForm(forms.ModelForm):
+  class Meta:
+    model=PDFdocuments
+    fields = ('description', 'document', )
